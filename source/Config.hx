7678b8d073f6d81df18b5a45dd370cdfcc072042
a-random441 <somerandomgithubacc@gmail.com>
2024-10-05 09:05:20 +0100

    fixes

--- source/Config.hx
+++ source/Config.hx
@@ -1,10 +1,20 @@
 package;
 
+import ui.FlxVirtualPad;
 import flixel.FlxG;
+import flixel.util.FlxSave;
+import flixel.math.FlxPoint;
 using StringTools;
 
 class Config
 {
+	var save:FlxSave;
+
+        public function new() 
+        {
+            save = new FlxSave();
+    	    save.bind("saveconrtol");
+        }
 	
 	public static var offset:Float;
 	public static var accuracy:String;
@@ -111,6 +121,57 @@ class Config
 
 		if(FlxG.save.data.ee1 == null)
 			FlxG.save.data.ee1 = false;
+	}
+	 public function getcontrolmode():Int {
+        // load control mode num from FlxSave
+		if (save.data.buttonsmode != null) return save.data.buttonsmode[0];
+        return 0;
+    }
+
+    public function setcontrolmode(mode:Int = 0):Int {
+        // save control mode num from FlxSave
+		if (save.data.buttonsmode == null) save.data.buttonsmode = new Array();
+        save.data.buttonsmode[0] = mode;
+        save.flush();
+
+        return save.data.buttonsmode[0];
+    }
+
+    public function savecustom(_pad:FlxVirtualPad) {
+		trace("saved");
+
+		if (save.data.buttons == null)
+		{
+			save.data.buttons = new Array();
+
+			for (buttons in _pad)
+			{
+				save.data.buttons.push(FlxPoint.get(buttons.x, buttons.y));
+			}
+		}else
+		{
+			var tempCount:Int = 0;
+			for (buttons in _pad)
+			{
+				save.data.buttons[tempCount] = FlxPoint.get(buttons.x, buttons.y);
+				tempCount++;
+			}
+		}
+		save.flush();
+	}
+
+	public function loadcustom(_pad:FlxVirtualPad):FlxVirtualPad {
+		//load pad
+		if (save.data.buttons == null) return _pad;
+		var tempCount:Int = 0;
+
+		for(buttons in _pad)
+		{
+			buttons.x = save.data.buttons[tempCount].x;
+			buttons.y = save.data.buttons[tempCount].y;
+			tempCount++;
+		}	
+        return _pad;
 	}
 	
 }
